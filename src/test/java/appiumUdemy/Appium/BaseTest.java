718eb0c92732d0fe8d4d2d60af9da6b6a61e00b5
Azima Keshwani <azima786@gmail.com>
2022-10-14 10:50:31 -0500

    update

--- src/test/java/appiumUdemy/Appium/BaseTest.java
+++ src/test/java/appiumUdemy/Appium/BaseTest.java
@@ -30,7 +30,8 @@ public class BaseTest {
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Azima-Pixel");
-        options.setApp("C://Users//azima.keshwani//Desktop//Appium//src//test//java//resources//ApiDemos-debug.apk");
+//        options.setApp("C://Users//azima.keshwani//Desktop//Appium//src//test//java//resources//ApiDemos-debug.apk");
+    options.setApp("C://Users//azima.keshwani//Desktop//Appium//src//test//java//resources//General-Store.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
